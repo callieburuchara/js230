af48e8c8708a4ff65e3a87c6accc2f4ec5e0757e
Callie Buruchara <callieburuchara@gmail.com>
2021-06-21 15:52:55 -0400

    Deleted Contact object (superfluous)

--- practice_project1/contact_manager_node/public/javascripts/functionality.js
+++ practice_project1/contact_manager_node/public/javascripts/functionality.js
@@ -10,36 +10,6 @@ Contact object
 - delete contact by id
 */
 
-class Contact {
-  constructor() {
-    this.contacts = [];
-    this.tags = [];
-  }
-
-  addContact(contact) {
-    this.contacts.push(contact);
-    return contact;
-  }
-
-  addTag(tag) {
-    this.tags.push(tag);
-    return tag;
-  }
-
-  getContactsByName(letters) {
-    return this.contacts.filter(contact => contact.name.includes(letters));
-  }
-
-  getContactsByTag(tag) {
-    return this.contacts.filter(contact => contact.tag === tag);
-  }
-
-  deleteContactById(id) {
-    let idx = this.contacts.findIndex(contact => contact.id === id);
-    return this.contacts.splice(idx, 1);
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   //Templates
   const scriptContacts = document.querySelector('#template-contacts');
